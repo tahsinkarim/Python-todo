0cffdd60ae2c8b0709ebe0b84adbbf39f0dec1bc
tahsinkarim <tahsinkarim.dev@gmail.com>
2024-07-10 17:57:49 +0600

    with-context-manager

--- main.py
+++ main.py
@@ -6,35 +6,36 @@ while True:
     match user_action:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
-            # Open file and write to store data
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
-        case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
 
-            print(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+        case 'show':
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
-                item = item.strip('\n')
+                item = item.strip('\n').capitalize()
                 row = f"{index + 1}-{item}"
                 print(row)
+
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+
         case 'complete':
             number = int(input("Number of the todo to complete: "))
             todos.pop(number - 1)
             print("Todo removed")
+
         case 'exit':
             break
 
