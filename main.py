6dd9cf45afdf783597e498b554b38b10c70697e1
tahsinkarim <tahsinkarim.dev@gmail.com>
2024-07-10 18:18:48 +0600

    added file read and write in edit and complete func

--- main.py
+++ main.py
@@ -28,14 +28,29 @@ while True:
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
-            print("Todo removed")
 
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip("\n")
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
         case 'exit':
             break
 
